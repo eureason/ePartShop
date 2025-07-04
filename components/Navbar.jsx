396274b0df81df1877b6fc9c2b079a921b0dc7de
eureason <eureason@gmail.com>
2025-07-04 20:21:15 +0900

    inngest problem

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
             className='flex items-center gap-2 hover:text-gray-900 transition'
           >
             <Image src={assets.user_icon} alt='user icon' />
-            Account
+            Acccount
           </button>
         )}
       </ul>
--- models/User.js
+++ models/User.js
@@ -6,11 +6,11 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
-    cartItems: { type: Object, required: true },
+    cartItems: { type: Object, default: {} },
   },
   { minimize: false }
 )
 
-const User = mongoose.models.User || mongoose.model('User', userSchema)
+const User = mongoose.models.user || mongoose.model('User', userSchema)
 
 export default User
